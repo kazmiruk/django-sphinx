a3205460150da5b8fd099e01cba97845632bf1db
kazmiruk <kazmiruk@live.ru>
2014-06-25 17:18:36 +0800

    fix encoding of utf

--- djangosphinx/models.py
+++ djangosphinx/models.py
@@ -654,8 +654,8 @@ class SphinxQuerySet(object):
             model_config = SPHINX_MODELS_FIELDS.get('%s.%s' % (instance._meta.app_label, instance._meta.object_name.lower()), {})
             index_name = model_config.get('index_name', None)
 
-            if isinstance(index_name, str):
-                index_name = index_name.decode('utf-8')
+            if isinstance(index_name, unicode):
+                index_name = index_name.encode('utf-8')
 
             fields_dict = model_config.get('fields', {})
             fields = fields_dict.keys()
@@ -676,8 +676,8 @@ class SphinxQuerySet(object):
             opts = self._passages_opts
         else:
             opts = {}
-        if isinstance(self._index, str):
-            self._index = self._index.decode('utf-8')
+        if isinstance(self._index, unicode):
+            self._index = self._index.encode('utf-8')
         passages_list = client.BuildExcerpts(docs, index_name or self._index, words, opts)
         
         passages = {}
